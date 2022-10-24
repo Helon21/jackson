ebfd2f29355ec358a1b67b0eee66e8279aa56d77
Luis Fernando Brasil <luis4756@live.com>
2022-10-24 01:19:05 -0300

    Lista Triggers(Em desenvolvimento)

---
+++ Listas Individuais/Luis Fernando/Lista Trigger.sql
@@ -0,0 +1,87 @@
+-- 01- Escreva quatro triggers de sintaxe - a trigger não precisa ter funcionalidade, basta não dar erro de sintaxe. Use variável global para testar.
+-- - Faça uma declarando variáveis e com select into; 
+DROP TRIGGER IF EXISTS exemplo_select_into;
+
+SET @teste = 'teste123';
+
+DELIMITER //
+CREATE TRIGGER exemplo_select_into
+AFTER INSERT ON item_venda
+FOR EACH ROW 
+BEGIN
+	SELECT nome INTO @teste FROM produto WHERE id = 1;
+END;
+//
+DELIMITER ;
+
+INSERT INTO item_venda(produto_id, venda_id, quantidade, preco_unidade) VALUES (1,1,3,10);
+SELECT @teste;
+-- - Faça a segunda com uma estrutura de decisão; 
+DROP TRIGGER IF EXISTS exemplo_if;
+
+SET @testeif = 'testando';
+
+DELIMITER //
+CREATE TRIGGER exemplo_if
+AFTER INSERT ON item_venda
+FOR EACH ROW 
+BEGIN
+	IF (1+1) = 2 THEN
+		BEGIN
+			SELECT nome INTO @teste FROM produto WHERE id = 1;
+		END;
+	END IF;
+END;
+//
+DELIMITER ;
+
+INSERT INTO item_venda(produto_id, venda_id, quantidade, preco_unidade) VALUES (1,1,3,10);
+SELECT @testeif;
+-- - Faça a terceira que gere erro, impedindo a ação;
+DROP TRIGGER IF EXISTS exemplo_erro;
+
+SET @testeif = 'testando';
+
+DELIMITER //
+CREATE TRIGGER exemplo_erro
+AFTER INSERT ON item_venda
+FOR EACH ROW 
+BEGIN
+	IF (curdate()>'2022-10-23') THEN
+		BEGIN
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'ERRO, PASSOU DA DATA LIMITE DAS INSERÇÕES';
+        END;
+	END IF;
+	SELECT nome INTO @teste FROM produto WHERE id = 1;
+END;
+//
+DELIMITER ;
+
+INSERT INTO item_venda(produto_id, venda_id, quantidade, preco_unidade) VALUES (1,1,3,10);
+SELECT @testeif;
+-- - Faça a quarta que utilize a variável new e old - tente diferenciar. 
+
+-- 02- Uma trigger que tem a função adicionar a entrada de produtos no estoque deve ser associado para qual:
+-- •	Tabela?
+-- •	Tempo?
+-- •	Evento?
+-- •	Precisa de variáveis? Quais?
+-- •	Implemente a trigger. 
+-- 03- Uma trigger que tem a função criar um registro de auditoria quando um pagamento e recebimento for alterada deve ser associado para qual(is):
+-- •	Tabela(s)?
+-- •	Tempo?
+-- •	Evento?
+-- •	Implemente a trigger (pode criar a tabela de auditoria)
+-- 04- Uma trigger que tem a função impedir a venda de um produto inferior a 50% do preço de venda deve ser associado para qual:
+-- •	Tabela?
+-- •	Tempo?
+-- •	Evento?
+-- •	Implemente a trigger
+-- 05- Este é para testar a sintaxe - tente implementar sem o script
+-- Uma trigger que tem a função de gerar o RA automático na tabela ALUNO deve ser associada para qual
+-- •	Tabela?
+-- •	Tempo?
+-- •	Evento?
+-- •	Precisa de variáveis? Quais?
+-- •	Implemente a trigger - RA igual a concatenção do ano corrente, código do curso e o id do cadastro do aluno. 
+-- 06- De acordo com o seu projeto de banco de dados, pense em pelo menos 3 trigger úteis. Discuta com os seus colegas em relação a relevância e implemente-as.
